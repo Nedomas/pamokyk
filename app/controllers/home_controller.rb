53b140f064fef79c3f4012ba5ee8eb69ec6991d5
Domas <domas.bitvinskas@me.com>
2014-05-19 00:19:31 +0300

    Add privacy policy

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -28,6 +28,10 @@ class HomeController < ApplicationController
     render json: true
   end
 
+  def privacy
+    render layout: false
+  end
+
   # def redirect
   #   session[:access_token] = Koala::Facebook::OAuth.new(oauth_redirect_url).get_access_token(params[:code]) if params[:code]
   #   @graph = Koala::Facebook::API.new(session[:access_token])
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Pamokyk::Application.routes.draw do
 
   # Example of regular route:
   get 'save' => 'home#save'
+  get 'privacy' => 'home#privacy'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
